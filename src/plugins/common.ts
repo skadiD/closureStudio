9add273390df7b267848f84799d1b34e6da25829
jackjieyyy <jackjieyyy@gmail.com>
2023-12-16 10:29:08 -0500

    refactor. assets item and stages

--- src/plugins/common.ts
+++ src/plugins/common.ts
@@ -43,11 +43,3 @@ export function getTheme() {
 
 export const isNight =
   new Date().getHours() >= 22 || new Date().getHours() <= 6;
-
-export const getStageName = (stages: Gamedata.Stages, stageId: string) => {
-  // check key is exist
-  if (!stages[stageId]) {
-    return "未知关卡";
-  }
-  return stages[stageId].name;
-};
