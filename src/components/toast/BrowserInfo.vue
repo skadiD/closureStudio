82820ab50dbee567898aad17edc28c9bf6f62594
jackjieyyy <jackjieyyy@gmail.com>
2023-12-29 02:46:29 -0500

    browser info

--- src/components/toast/BrowserInfo.vue
+++ src/components/toast/BrowserInfo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="toast toast-center z-10">
+    <div class="toast toast-center z-10 [@supports(color:oklch(0_0_0))]:hidden">
         <div class="alert bg-black grid-cols-[auto] mb-2 py-2 text-white text-xl">
             <span>
                 <a class="link" rel="nofollow, noreferrer" target="_blank" href="https://www.wikihow.com/Update-Your-Browser"> 请使用现代浏览器(chrome firefox等)</a>
