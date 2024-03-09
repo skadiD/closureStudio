e0bb462a3e4251980509e3d4ac28ae80c8f305e3
jackjieyyy <jackjieyyy@gmail.com>
2024-03-09 10:03:50 -0500

    admin account

--- src/plugins/quota/quota.ts
+++ src/plugins/quota/quota.ts
@@ -1,60 +1,55 @@
 export interface canAddGameResult {
-  message: string;
-  isLocked: boolean;
+    message: string;
+    isLocked: boolean;
 }
 
-export const canDeleteGame = (
-  userQuota: Registry.UserInfo,
-  gameAccount: string
-) => {
-  if (!userQuota) {
-    return false;
-  }
-  if (userQuota.idServerPhone.toString() === gameAccount) {
-    return false;
-  }
-  if (userQuota.idServerPhone.toString() === "") {
+export const canDeleteGame = (userQuota: Registry.UserInfo, gameAccount: string) => {
+    if (!userQuota) {
+        return false;
+    }
+    if (userQuota.idServerPhone.toString() === gameAccount) {
+        return false;
+    }
+    if (userQuota.idServerPhone.toString() === "") {
+        return true;
+    }
     return true;
-  }
-  return true;
 };
 
-export const allowGameCreate = (
-  slot: Registry.Slot,
-  userQuota: Registry.UserInfo,
-  isVerify: boolean
-) => {
-  let response: canAddGameResult = {
-    message: "请完成手机号绑定",
-    isLocked: true,
-  };
-  if (!userQuota) {
-    return response;
-  }
-  if (
-    slot.ruleFlags.includes("slot_account_format_is_phone") &&
-    slot.ruleFlags.includes("slot_account_sms_verified")
-  ) {
-    response.message = "添加第一个托管";
-    response.isLocked = false;
-    return response;
-  }
-
-  if (slot.ruleFlags.includes("slot_user_sms_verified") && isVerify) {
-    response.message = "添加游戏托管";
-    response.isLocked = false;
-    return response;
-  }
-
-  if (slot.ruleFlags.includes("slot_user_qq_verified") && isVerify) {
-    if (!userQuota.idServerQQ) {
-      response.message = "请完成QQ绑定";
-      return response;
-    } else {
-      response.message = "添加游戏托管";
-      response.isLocked = false;
-      return response;
+export const allowGameCreate = (slot: Registry.Slot, userQuota: Registry.UserInfo, isVerify: boolean) => {
+    let response: canAddGameResult = {
+        message: "请完成手机号绑定",
+        isLocked: true
+    };
+    if (!userQuota) {
+        return response;
     }
-  }
-  return response;
+    if (slot.ruleFlags.includes("slot_account_format_is_phone") && slot.ruleFlags.includes("slot_account_sms_verified")) {
+        response.message = "添加第一个托管";
+        response.isLocked = false;
+        return response;
+    }
+
+    if (slot.ruleFlags.includes("slot_user_sms_verified") && isVerify) {
+        response.message = "添加游戏托管";
+        response.isLocked = false;
+        return response;
+    }
+
+    if (slot.ruleFlags.includes("slot_user_qq_verified") && isVerify) {
+        if (!userQuota.idServerQQ) {
+            response.message = "请完成QQ绑定";
+            return response;
+        } else {
+            response.message = "添加游戏托管";
+            response.isLocked = false;
+            return response;
+        }
+    }
+    if (slot.ruleFlags.length === 0 && isVerify) {
+        response.message = "添加游戏托管";
+        response.isLocked = false;
+        return response;
+    }
+    return response;
 };
