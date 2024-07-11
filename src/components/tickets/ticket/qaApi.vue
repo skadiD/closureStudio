ffb4d2d594386342f0376abf5e9394e6c5669180
jackjieYYY <jackjieYYY@gmail.com>
2024-07-11 14:27:53 +1000

    qaApi

--- src/components/tickets/ticket/qaApi.vue
+++ src/components/tickets/ticket/qaApi.vue
@@ -1,45 +1,53 @@
 <template>
-    <div>
-        <h1>问答服务</h1>
-        <button @click="sendRequest">开始问答</button>
-        <div v-for="message in messages" :key="message.id">
-            {{ message.type }}: {{ message.data }}
-        </div>
+  <div>
+    <h1>问答服务</h1>
+    <button @click="sendRequest">开始问答</button>
+    <div v-for="message in messages" :key="message.id">
+      {{ message.type }}: {{ message.data }}
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onUnmounted, ref } from 'vue';
-import axios from 'axios';
-import fs from 'fs'
+import { defineComponent, ref } from "vue";
+import axios from "axios";
 export default defineComponent({
-    name: 'QaSseClient',
-    setup() {
-        const messages = ref<Array<{ id: number; type: string; data: string }>>([]);
-        async function sendRequest() {
-            const response = axios({
-                method: 'post',
-                url: 'https://inference.ltsc.vip/api/chat/query',
-                data: { query: '可露西尔云平台支持自动打肉鸽吗？', stream: true },
-                headers: {
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFya25pZ2h0cy5ob3N0IiwiZXhwIjoxNzIwMzQzMjA1LCJpc0FkbWluIjpmYWxzZSwicGVybWlzc2lvbiI6MSwic3RhdHVzIjoyLCJ1dWlkIjoiNDkyZjYwMjUtZTVkYS00YWQwLTljZWQtZGUyYTY0MTgxNmQ3In0.pFRSDhW6vGMz9MVEuxjT2Ra-RMnL96mpKOtSXSGyJLw'
-                },
-                responseType: 'stream'
-            })
+  name: "QaSseClient",
+  setup() {
+    const messages = ref<Array<{ id: number; type: string; data: string }>>([]);
+    const sendRequest = () => {
+      const user = localStorage.getItem("closureV3_user");
+      if (!user) return;
+      axios
+        .post(
+          "https://api.chat.ltsc.vip/api/chat/query",
+          { query: "可露西尔云平台支持自动打肉鸽吗？", stream: true },
+          {
+            headers: {
+              Accept: "text/event-stream",
+              Authorization: "Bearer " + JSON.parse(user)?.user?.Token,
+            },
+            responseType: "stream",
+            adapter: "fetch", // <- this option can also be set in axios.create()
+          }
+        )
+        .then(async (response) => {
+          console.log("axios got a response");
+          const stream = response.data;
+          // consume response
+          const reader = stream
+            .pipeThrough(new TextDecoderStream())
+            .getReader();
+          while (true) {
+            const { value, done } = await reader.read();
+            if (done) break;
+            console.log(value);
+          }
+        });
+      // catch/etc.
+    };
 
-            // const stream = response.data;
-            // // `progress`, `set_text`, `delta`, `end`
-
-            // stream.on('delta', (data: any) => {
-            //     console.log(data);
-            // });
-
-            // stream.on('end', () => {
-            //     console.log("stream done");
-            // });
-        }
-
-        return { messages, sendRequest };
-    },
+    return { messages, sendRequest };
+  },
 });
 </script>
